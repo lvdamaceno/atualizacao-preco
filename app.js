260bf639dcace745e55d508d0e420af4d320f276
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2024-02-16 16:56:01 -0300

    update remove send message

--- app.js
+++ app.js
@@ -13,48 +13,39 @@ const config = {
     options: { encrypt: false }
 };
 
-function sendMessage(codprod, vlrvenda) {
-    fetch('https://graph.facebook.com/v18.0/183582418183115/messages', {
-        method: 'POST',
-        headers: {
-            'Authorization': process.env.TOKEN,
-            'Content-Type': 'application/json',
-            'Cookie': 'ps_l=0; ps_n=0'
-        },
-        body: JSON.stringify({
-            "messaging_product": "whatsapp",
-            "to": process.env.PHONE,
-            "type": "template",
-            "template": {
-                "name": "atualizacao_de_preco",
-                "language": {
-                    "code": "pt_BR"
-                },
-                "components": [{
-                    "type": "body",
-                    "parameters": [{
-                        "type": "text",
-                        "text": codprod
-                    }, {
-                        "type": "text",
-                        "text": vlrvenda
+async function sendMessage(codprod, vlrvenda) {
+    try {
+        const response = await fetch('https://graph.facebook.com/v18.0/183582418183115/messages', {
+            method: 'POST',
+            headers: {
+                'Authorization': process.env.TOKEN,
+                'Content-Type': 'application/json',
+                'Cookie': 'ps_l=0; ps_n=0'
+            },
+            body: JSON.stringify({
+                "messaging_product": "whatsapp",
+                "to": process.env.PHONE,
+                "type": "template",
+                "template": {
+                    "name": "atualizacao_de_preco",
+                    "language": { "code": "pt_BR" },
+                    "components": [{
+                        "type": "body",
+                        "parameters": [{ "type": "text", "text": codprod }, { "type": "text", "text": vlrvenda }]
                     }]
-                }]
-            }
-        })
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Erro ao executar a solicitação');
-            }
-            return response.json(); // Retorna os dados em formato JSON
-        })
-        .then(data => {
-            console.log(data); // Faz algo com os dados recebidos
-        })
-        .catch(error => {
-            console.error('Houve um erro:', error);
+                }
+            })
         });
+
+        if (!response.ok) {
+            throw new Error('Erro ao executar a solicitação');
+        }
+
+        const data = await response.json();
+        console.log(data); // Faz algo com os dados recebidos
+    } catch (error) {
+        console.error('Houve um erro:', error);
+    }
 }
 
 function saveToBaseAndSendMessage(nutab, codprod, vlrvenda, newvlrvenda) {
@@ -69,33 +60,29 @@ function saveToBaseAndSendMessage(nutab, codprod, vlrvenda, newvlrvenda) {
                     return console.error(err.message);
                 }
                 console.log(`O produto ${codprod} foi atualizado para o valor ${vlrvenda}`);
-                sendMessage(codprod, vlrvenda)
-                console.log("Mensagem enviada")
+                // sendMessage(codprod, vlrvenda)
             })
         } else {
-            // console.log(`O produto ${codprod} já existe na tabela com valor ${vlrvenda}`);
+            console.log(`O produto ${codprod} já existe na tabela com valor ${vlrvenda}`);
         }
     })
 }
 
 // Função de conexão e execução de consulta
 async function executeQuery() {
-
     try {
         // Conecta ao banco de dados
         await sql.connect(config);
         // Executa uma consulta
         const result = await sql.query(process.env.QUERY);
-
         const len = result.recordsets[0].length
-
+        console.log('Conectando...')
         for (let i = 0; i < len; i++) {
             var nutab = result.recordset[i].NUTAB
             var codprod = result.recordset[i].CODPROD
             var vlrvenda = result.recordset[i].VLRVENDA
             var newvlrvenda = result.recordset[i].VLRVENDA
             saveToBaseAndSendMessage(nutab, codprod, vlrvenda, newvlrvenda)
-
         }
     } catch (err) {
         console.log(err); // Exibe erros, se houverem
@@ -106,8 +93,13 @@ async function executeQuery() {
     }
 }
 
+// Definindo a função que você deseja executar
+function minhaFuncao() {
+    console.log('Executando minha função...');
+    // Adicione o código que você deseja executar aqui
+}
+
+// Executar a função a cada segundo usando setInterval
+// setInterval(executeQuery, 15000); // 1000 milissegundos = 1 segundo
+
 executeQuery()
-
-
-
-
