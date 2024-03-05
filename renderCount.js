b49e2aa21c82e8b8040226c329a008654903b949
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2024-03-05 15:03:31 -0300

    create count and drop

---
+++ renderCount.js
@@ -0,0 +1,32 @@
+var { Client } = require('pg');
+require('dotenv').config();
+
+var dbConn = process.env.DB_URL;
+
+const client = new Client(dbConn);
+client.connect()
+  .then(() => {
+    console.log('Connected to PostgreSQL database');
+
+    // Execute SQL queries here
+
+    client.query("SELECT COUNT (*) as total FROM prices", (err, result) => {
+      if (err) {
+        console.error('Error executing query', err);
+      } else {
+        console.log('Query result:', result.rows);
+      }
+
+      // Close the connection when done
+      client.end()
+        .then(() => {
+          console.log('Connection to PostgreSQL closed');
+        })
+        .catch((err) => {
+          console.error('Error closing connection', err);
+        });
+    });
+  })
+  .catch((err) => {
+    console.error('Error connecting to PostgreSQL database', err);
+  });
